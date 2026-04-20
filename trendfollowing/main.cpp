2bef33d5594a94bd7429023eaf877a92763a4315
BohanHou <houbohan0303@gmail.com>
2026-04-20 16:18:45 -0400

    Initial Commit

---
+++ trendfollowing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  trendfollowing
+//
+//  Created by Bohan Hou on 2026-04-20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
